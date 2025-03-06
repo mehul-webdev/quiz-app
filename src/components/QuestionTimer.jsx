efbaec4ebf52186fc6f3799eb6e1e85f06b4e2e4
mehul <mehulchaudhary330@gmail.com>
2025-03-06 18:25:01 +0530

    updared code

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-const QuestionTimer = ({ timeout, onTimeout }) => {
+const QuestionTimer = ({ timeout, onTimeout, mode }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      onTimeout();
+      if (onTimeout) {
+        onTimeout();
+      }
     }, timeout);
 
     return () => {
@@ -23,7 +25,14 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
     };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 };
 
 export default QuestionTimer;
